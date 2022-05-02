d593ae25134b247839b10f943f668cc2af020bde
xhrg <634789257@qq.com>
2022-05-02 21:30:18 +0800

    init

---
+++ src/main/java/io/github/xhrg/basic/core/basiccore/BasicCoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.xhrg.basic.core.basiccore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCoreApplication.class, args);
+	}
+
+}
